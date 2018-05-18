c6b693a56321b471884cc80360b66e2370bb1ddd
Yakup <yakup.ogurall@gmail.com>
2018-05-18 18:35:12 +0300

    bilal

--- app/src/main/java/com/example/yakup/myapplication2/ggv.java
+++ app/src/main/java/com/example/yakup/myapplication2/ggv.java
@@ -8,4 +8,7 @@ public class ggv {
     private String bilal;
     private String bilal;
     private String bilal;
+    private String bilal;
+    private String bilal;
+    private String bilal;
 }
